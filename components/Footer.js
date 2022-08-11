75974f50f447830b2c6c3dbdac3f2c0ee33c04e3
rcos7 <radu.costica@parity.io>
2022-08-11 10:04:27 +0100

    add border to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,14 @@
-import { Link } from 'gatsby';
-import React from 'react';
+import { Link } from "gatsby";
+import React from "react";
 
-import { Icon } from './Icon';
-import { NavFooter } from './NavFooter';
-import { NavLegal } from './NavLegal';
-import { Newsletter } from './Newsletter';
+import { Icon } from "./Icon";
+import { NavFooter } from "./NavFooter";
+import { NavLegal } from "./NavLegal";
+import { Newsletter } from "./Newsletter";
 
 const Footer = ({ showNewsletter = true }) => {
   return (
-    <footer className="w-full px-4 xl:px-12 bg-substrateDark text-white font-body">
+    <footer className="w-full px-4 xl:px-12 bg-substrateDark text-white font-body border-t border-gray-200 dark:border-gray-700">
       {showNewsletter && <Newsletter layout="default" />}
       <div className="lg:flex lg:flex-row-reverse lg:justify-between lg:pt-12 lg:pb-3 border-b-2 border-gray-600">
         <div className="w-52 lg:h-7 relative py-12 lg:p-0 transform transition-all duration-300 ease-in-out hover:opacity-50">
