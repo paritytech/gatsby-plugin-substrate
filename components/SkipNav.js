9c579021a6235691716c33f12ebb674bc21a82f0
je-boska <joneirikboska@gmail.com>
2022-10-21 11:02:55 +0200

    Add SkipNav button

---
+++ components/SkipNav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function SkipNav({ main }) {
+  return (
+    <a
+      onClick={e => {
+        e.preventDefault();
+        main.current.focus({ preventScroll: true });
+      }}
+      href=""
+      className="fixed -top-20 left-4 xl:left-8 z-50 focus:top-5 py-3 px-6 bg-white dark:bg-substrateBlackish rounded"
+    >
+      Skip navigation
+    </a>
+  );
+}
