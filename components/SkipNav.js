481dad5b11ea371b0beda7d3a9b3591785f6a479
je-boska <joneirikboska@gmail.com>
2022-10-21 15:08:53 +0200

    Make SkipNav a button element instead of link

--- components/SkipNav.js
+++ components/SkipNav.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 export default function SkipNav({ main }) {
   return (
-    <a
+    <button
       onClick={e => {
         e.preventDefault();
-        main.current.focus({ preventScroll: true });
+        if (main && main.current) {
+          main.current.focus({ preventScroll: true });
+        }
       }}
-      href=""
       className="fixed -top-20 left-4 xl:left-8 z-50 focus:top-5 py-3 px-6 bg-white dark:bg-substrateBlackish rounded"
     >
       Skip navigation
-    </a>
+    </button>
   );
 }
