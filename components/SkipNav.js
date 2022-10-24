c718e92962ccddeea87586bbe0ee2591e393ffe1
je-boska <joneirikboska@gmail.com>
2022-10-24 15:06:10 +0200

    Clean up code a bit

--- components/SkipNav.js
+++ components/SkipNav.js
@@ -3,13 +3,12 @@ import React from 'react';
 export default function SkipNav({ main }) {
   return (
     <button
-      onClick={e => {
-        e.preventDefault();
+      onClick={() => {
         if (main && main.current) {
           main.current.focus({ preventScroll: true });
         }
       }}
-      className="fixed -top-20 left-4 xl:left-8 z-50 focus:top-5 py-3 px-6 bg-white dark:bg-substrateBlackish rounded shadow-md"
+      className="fixed z-50 -top-20 left-4 xl:left-8 focus:top-5 py-3 px-6 bg-white dark:bg-substrateBlackish rounded shadow-md"
     >
       Skip navigation
     </button>
